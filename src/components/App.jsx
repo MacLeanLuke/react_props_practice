b4cf03c30e9de37da85934d7005cf88da3027cd9
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2021-01-25 18:00:23 -0500

    create card component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,76 @@
 import React from "react";
+import Card from "./Card";
 
 function App() {
   return (
     <div>
-      <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      <h1 className="heading">Dunder Mifflin Scranton Contact Us</h1>
+      <Card
+        name="Michael Scott"
+        img="https://miro.medium.com/max/500/1*xDIevNE7HEMiJQVTYg0qDQ.png"
+        tel="308 456 7890"
+        email="threatLevelMidnight@dundermifflin.com"
+      />
+      <Card
+        name="Dwight Schrute"
+        img="https://dundermifflinscrantonbranch.weebly.com/uploads/1/4/9/0/14905642/864013.jpg?156"
+        tel="308-826-5962"
+        email="beetfarmer97@dundermifflin.com"
+      />
+      <Card
+        name="Jim Halpert"
+        img="https://dundermifflinscrantonbranch.weebly.com/uploads/1/4/9/0/14905642/6349758.jpg?216"
+        tel="308-726-7327"
+        email="whoPutMyStaplerInJello@dundermifflin.com"
+      />
+      <Card
+        name="Pam Beesley"
+        img="https://dundermifflinscrantonbranch.weebly.com/uploads/1/4/9/0/14905642/9673001.jpg?161"
+        tel="308-512-7656"
+        email="watercolorArtist@dundermifflin.com"
+      />
+      <Card
+        name="Creed Bratton"
+        img="https://dundermifflinscrantonbranch.weebly.com/uploads/1/4/9/0/14905642/4752739.jpg?158"
+        tel="308-735-6808"
+        email="somebodyMakinSoup@dundermifflin.com"
+      />
+      <Card
+        name="Angela Martin"
+        img="https://dundermifflinscrantonbranch.weebly.com/uploads/1/4/9/0/14905642/7916276.jpg?156"
+        tel="308-564-2597"
+        email="iLoveSprinkles@dundermifflin.com"
+      />
+      <Card
+        name="Toby Flenderson"
+        img="https://dundermifflinscrantonbranch.weebly.com/uploads/1/4/9/0/14905642/3541566.jpg?174"
+        tel="308-123-4567"
+        email="tflenderson@dundermifflin.com"
+      />
+      <Card
+        name="Kevin Malone"
+        img="https://dundermifflinscrantonbranch.weebly.com/uploads/1/4/9/0/14905642/5356844.jpg?157"
+        tel="308-989-4245"
+        email="chiliChef@dundermifflin.com"
+      />
+      <Card
+        name="Andy Bernard"
+        img="https://dundermifflinscrantonbranch.weebly.com/uploads/1/4/9/0/14905642/8200098.jpg?157"
+        tel="308-786-4983"
+        email="narddog@dundermifflin.com"
+      />
+      <Card
+        name="Ryan the Temp"
+        img="https://dundermifflinscrantonbranch.weebly.com/uploads/1/4/9/0/14905642/7133812.jpg?157"
+        tel="308-412-0374"
+        email="dunderWonder@dundermifflin.com"
+      />
+      <Card
+        name="Stanley Hudson"
+        img="https://dundermifflinscrantonbranch.weebly.com/uploads/1/4/9/0/14905642/6152574.jpg?222"
+        tel="308-525-8868"
+        email="didIStutter@dundermifflin.com"
+      />
     </div>
   );
 }
